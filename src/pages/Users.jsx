0d79a5441f90311c425f2ff48013012c697c935a
HamzaHamani <hamzahamani2004@gmail.com>
2023-12-05 02:44:04 +0100

    fixed issue for deplying

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,11 +1,11 @@
 import Heading from "../ui/Heading";
-import SignUpForm from "../features/authentication/SignUpForm";
+import SignForm from "../features/authentication/SignForm";
 
 function NewUsers() {
   return (
     <>
       <Heading as="h1">Add a new memeber to the team</Heading>
-      <SignUpForm />
+      <SignForm />
     </>
   );
 }
